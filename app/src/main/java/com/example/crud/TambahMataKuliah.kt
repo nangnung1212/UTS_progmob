213b2ee15996fd38c639cab07f98b3ae9eb55ab9
Gunung <arizonagabriel12@gmail.com>
2021-10-22 16:47:42 +0700

    UTS SELESAI

---
+++ app/src/main/java/com/example/crud/TambahMataKuliah.kt
@@ -0,0 +1,11 @@
+package com.example.crud
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TambahMataKuliah : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tambah_mata_kuliah)
+    }
+}
